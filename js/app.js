5f9e3c04fca4f29127174fdd2978fda25f6b9af1
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-22 11:07:04 +0300

    burger menu fix

--- js/app.js
+++ js/app.js
@@ -2,36 +2,30 @@ import { isWebp } from './components/isWebp.js'
 
 isWebp()
 
-const $burgerButton = document.querySelector('.header__top-burger')
+const $burgerButtons = document.querySelectorAll('.header__top-burger')
 const $burgerMenu = document.querySelector('.burger__menu')
+const $body = document.querySelector('body')
+const $html = document.querySelector('html')
 
-const $headerLogo = document.querySelector('.header__logo')
-const $headerLogoImg = document.querySelector('.header__logo-img')
-const $headerTop = document.querySelector('.header__top')
+$burgerButtons.forEach((btn) => {
+	btn.addEventListener('click', () => {
+		$burgerButtons.forEach((btn) => btn.classList.toggle('active'))
+		$burgerMenu.classList.toggle('opened')
 
-$burgerButton.addEventListener('click', () => {
-	let headerMargin = parseInt(window.getComputedStyle($headerLogo, null).getPropertyValue('margin-top'))
-	let logoHeight = parseInt(window.getComputedStyle($headerLogoImg, null).getPropertyValue('height'))
-	let headerTopHeight = parseInt(window.getComputedStyle($headerTop, null).getPropertyValue('height'))
-	let padding = headerMargin + logoHeight + headerTopHeight - 20
-	$burgerButton.classList.toggle('active')
-	$burgerMenu.classList.toggle('opened')
-	if ($burgerMenu.classList.contains('opened')) {
-		setTimeout(() => {
-			$burgerButton.style.cssText = `background-color: #0069E4; width: 100%; padding: ${padding}px 0;`
-		}, 500)
-	} else {
-		$burgerButton.style.cssText = ``
-	}
-	document.body.classList.toggle('no-scroll')
-})
+		if ($body.classList.contains('no-scroll')) {
+			setTimeout(() => {
+				$body.classList.remove('no-scroll')
+			}, 300)
+		} else {
+			$body.classList.add('no-scroll')
+		}
 
-window.addEventListener('resize', () => {
-	let headerMargin = parseInt(window.getComputedStyle($headerLogo, null).getPropertyValue('margin-top'))
-	let logoHeight = parseInt(window.getComputedStyle($headerLogoImg, null).getPropertyValue('height'))
-	let headerTopHeight = parseInt(window.getComputedStyle($headerTop, null).getPropertyValue('height'))
-	let padding = headerMargin + logoHeight + headerTopHeight - 20
-	if ($burgerMenu.classList.contains('opened')) {
-		$burgerButton.style.cssText = `background-color: #0069E4; width: 100%; padding: ${padding}px 0;`
-	}
+		if ($html.classList.contains('no-scroll')) {
+			setTimeout(() => {
+				$html.classList.remove('no-scroll')
+			}, 300)
+		} else {
+			$html.classList.add('no-scroll')
+		}
+	})
 })
