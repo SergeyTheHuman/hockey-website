345d3b18914514572d5e301482a0fdc09e233acf
SergeyTheHuman <sergey2611.ivanov@yandex.ru>
2022-05-27 16:03:40 +0300

    added popups

--- js/app.js
+++ js/app.js
@@ -7,6 +7,53 @@ const $burgerButtons = document.querySelectorAll('.header__top-burger')
 const $burgerMenu = document.querySelector('.burger__menu')
 const $body = document.querySelector('body')
 const $backToHomeBtn = document.querySelector('.thanks__button')
+const $dateInput = document.querySelector('.form__date')
+const $bookingModal = document.querySelector('.ice__booking-wrapper')
+const $connectModal = document.querySelector('.connect__us-wrapper')
+const $popupButtons = document.querySelectorAll('[data-modal]')
+const $closeModalButtons = document.querySelectorAll('.form__close')
+
+const $modals = [$bookingModal, $connectModal]
+
+$modals.forEach((modal) => {
+	modal.addEventListener('click', (e) => {
+		if (!e.target.closest('.form__body')) {
+			$bookingModal.classList.remove('opened-modal')
+			$connectModal.classList.remove('opened-modal')
+		}
+		if (e.target.classList.contains('form__close')) {
+			$bookingModal.classList.remove('opened-modal')
+			$connectModal.classList.remove('opened-modal')
+		}
+	})
+})
+// $closeModalButtons.forEach((btn) => {
+// 	btn.addEventListener('click', (e) => {
+// 		e.preventDefault()
+// 		if (!e.target.closest('.form')) {
+// 			console.log('ЗАКРЫТЬ ААААААААААААААААААА')
+// 		}
+// 		$bookingModal.classList.remove('opened-modal')
+// 		$connectModal.classList.remove('opened-modal')
+// 	})
+// })
+
+$popupButtons.forEach((btn) => {
+	btn.addEventListener('click', (e) => {
+		let dataModal = btn.dataset.modal
+		e.preventDefault()
+		if ($burgerMenu.classList.contains('opened')) {
+			$burgerMenu.classList.toggle('opened')
+
+			$body.classList.contains('no-scroll') ? unlock($body) : lock($body)
+		}
+		if (dataModal === 'connectModal') {
+			$connectModal.classList.add('opened-modal')
+		} else if (dataModal === 'bookingModal') {
+			$bookingModal.classList.add('opened-modal')
+		}
+	})
+})
 
 function lock(element) {
 	const lockPadding = window.innerWidth - $body.clientWidth + 'px'
@@ -19,7 +66,16 @@ function unlock(element) {
 	element.classList.remove('no-scroll')
 	element.style.paddingRight = ''
 }
-
+if ($dateInput) {
+	$dateInput.addEventListener('focus', () => {
+		$dateInput.setAttribute('type', 'date')
+	})
+	$dateInput.addEventListener('blur', () => {
+		if ($dateInput.value === '') {
+			$dateInput.setAttribute('type', 'text')
+		}
+	})
+}
 if ($backToHomeBtn) {
 	$backToHomeBtn.addEventListener('click', () => {
 		window.location.href = `index.html`
